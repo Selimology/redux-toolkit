40ce1ee93aa689ce688a57a6282d262b9d8e16b8
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-11-06 22:51:42 +0100

    async redux-toolkit createAsyncThunk

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,32 +1,44 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { postAdded } from './postsSlice';
+
+import { addNewPost } from './postsSlice';
 import { selectAllUsers } from '../users/usersSlice';
 
 const AddPostForm = () => {
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [userId, setUserId] = useState('');
+  const [addRequestStatus, setAddRequestStatus] = useState('idle');
 
   const users = useSelector(selectAllUsers);
-  const dispatch = useDispatch();
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
+
   const onSavePostClicked = () => {
     if (canSave) {
-      dispatch(postAdded(title, content, userId));
-    }
+      try {
+        setAddRequestStatus('pending');
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
 
-    setTitle('');
-    setContent('');
+        setTitle('');
+        setContent('');
+        setUserId('');
+      } catch (err) {
+        console.error('Failed to save the post', err);
+      } finally {
+        setAddRequestStatus('idle');
+      }
+    }
   };
 
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
-
-  const userOptions = users.map((user) => (
+  const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
@@ -34,11 +46,11 @@ const AddPostForm = () => {
 
   return (
     <section>
-      <h2>Add a New Post!</h2>
+      <h2>Add a New Post</h2>
       <form>
-        <label htmlFor="postTitle"> Post Title:</label>
+        <label htmlFor="postTitle">Post Title:</label>
         <input
-          type="type"
+          type="text"
           id="postTitle"
           name="postTitle"
           value={title}
@@ -47,21 +59,20 @@ const AddPostForm = () => {
         <label htmlFor="postAuthor">Author:</label>
         <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
           <option value=""></option>
-          {userOptions}
+          {usersOptions}
         </select>
-        <label htmlFor="postContent"> Post Content:</label>
+        <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
           name="postContent"
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" disabled={!canSave} onClick={onSavePostClicked}>
-          Save Button
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
         </button>
       </form>
     </section>
   );
 };
-
 export default AddPostForm;
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,25 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [
-  {
-    id: '0',
-    name: 'Kamil Ertekin',
-  },
-  {
-    id: '1',
-    name: 'Latif Ertekin',
-  },
-  {
-    id: '2',
-    name: 'Feyza Ertekin',
-  },
-];
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
+
 export default usersSlice.reducer;
